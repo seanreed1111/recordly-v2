1f5783a1f39805cd8742f91200f7eb578495088c
seanreed1111 <seanreed1111@gmail.com>
2016-08-16 16:40:34 -0400

    more empty layout framework. no code yet

--- db/migrate/20160816202450_add_favorite_to_artists_albums_songs.rb
+++
@@ -1,7 +0,0 @@
-class AddFavoriteToArtistsAlbumsSongs < ActiveRecord::Migration
-  def change
-    add_column :albums, :favorite, :boolean, null: false, default: false
-    add_column :artists, :favorite, :boolean, null: false, default: false
-    add_column :songs, :favorite, :boolean, null: false, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,31 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816202450) do
+ActiveRecord::Schema.define(version: 20160816200638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "favorite",   default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "favorite",   default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "songs", force: :cascade do |t|
     t.string   "name"
     t.integer  "album_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "favorite",   default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "songs", ["album_id"], name: "index_songs_on_album_id", using: :btree
