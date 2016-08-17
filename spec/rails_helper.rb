ac829af9e17b3dd106362da9b8fdbd708671746d
seanreed1111 <seanreed1111@gmail.com>
2016-08-16 23:08:36 -0400

    fixes warden error in controller spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,7 +48,6 @@ require 'spec_helper'
     # The different available types are documented in the features, such as in
     # https://relishapp.com/rspec/rspec-rails/docs
     config.infer_spec_type_from_file_location!
-
     # Filter lines from Rails gems in backtraces.
     config.filter_rails_from_backtrace!
     # arbitrary gems may also be filtered via:
@@ -60,7 +59,7 @@ require 'spec_helper'
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
-
+    config.include Devise::Test::ControllerHelpers, type: :controller
     config.use_transactional_fixtures = false
 
     config.before(:suite) do
